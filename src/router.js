539272f309b828eaf902a7c66aec6a2aaf44ff6a
amirsahra <amirhosein.sahra@gmail.com>
2023-02-23 12:36:58 +0330

    feat: users tmp

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from "@/components/pages/Home";
+import Users from "@/components/pages/user/Users";
 
 const routes = [
-    {path: "/", name: "home", component: Home}
+    {path: "/", name: "home", component: Home},
+    {path: "/users", name: "users", component: Users}
 ];
 
 const router = createRouter({
