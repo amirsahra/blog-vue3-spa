506c0e7a45832de78047bdcced982b6f1f1f37cc
amirsahra <amirhosein.sahra@gmail.com>
2023-02-23 10:42:12 +0330

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
-  </div>
+  <Header/>
   <router-view></router-view>
 </template>
 
 <script>
-
+import Header from "@/components/Header";
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
