47c865b2481af8960b4967d9f8234fd31a30d9d1
amirsahra <amirhosein.sahra@gmail.com>
2023-02-27 12:34:13 +0330

    feat: post feature

--- src/components/sections/Post-Feature.vue
+++ src/components/sections/Post-Feature.vue
@@ -1,27 +1,46 @@
 <template>
-  <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top feat-img" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-    <div class="card-body">
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-muted">9 mins</small>
+  <div>
+    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+      <div class="col p-4 d-flex flex-column position-static">
+        <h3 class="mb-0">{{ post.title }}</h3>
+        <div class="mb-1 text-muted">{{ date }}</div>
+        <a href="#" class="stretched-link">Continue reading</a>
+      </div>
+      <div class="col-auto d-none d-lg-block">
+        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img"
+             aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>
+          Placeholder</title>
+          <rect width="100%" height="100%" fill="#55595c"></rect>
+        </svg>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {ref} from "vue";
+
 export default {
-  name: "Post-Feature"
+  name: "Post-Feature",
+  props: ['post'],
+  setup() {
+    const date = ref({})
+
+    function getRandomDate() {
+      const maxDate = Date.now();
+      const timestamp = Math.floor(Math.random() * maxDate);
+      return new Date(timestamp);
+    }
+
+    date.value= getRandomDate();
+    return {date};
+  }
+
 }
 </script>
 
 <style scoped>
-.feat-img{
-  padding-top: 10px;
+.feat-img {
+  padding: 10px;
 }
 </style>
